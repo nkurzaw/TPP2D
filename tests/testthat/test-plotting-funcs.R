4e4d848706a7240817a058f61bcf9593aadd7e3a
Nils Kurzawa <nilskurzawa@Mac.fritz.box>
2025-09-18 21:44:29 +0200

    bug fix: fix tests due to ggplot class change

--- tests/testthat/test-plotting-funcs.R
+++ tests/testthat/test-plotting-funcs.R
@@ -4,16 +4,19 @@ context("plot functions")
 
 test_that("plot2dTppProfile generates ggplot object", {
   gg_profile <- plot2dTppProfile(simulated_cell_extract_df, "protein1")
-  expect_identical(class(gg_profile), c("gg", "ggplot"))
+  expect_identical(class(gg_profile), c("ggplot2::ggplot", "ggplot", 
+                                        "ggplot2::gg", "S7_object", "gg"))
 })
 
 test_that("plot2dTppProfile generates ggplot object", {
   gg_rel_profile <- plot2dTppRelProfile(simulated_cell_extract_df, "protein1")
-  expect_identical(class(gg_rel_profile), c("gg", "ggplot"))
+  expect_identical(class(gg_rel_profile), c("ggplot2::ggplot", "ggplot", 
+                                            "ggplot2::gg", "S7_object", "gg"))
 })
 
 test_that("plot2dTppFcHeatmap generates ggplot object", {
     gg_rel_heatmap <- plot2dTppFcHeatmap(
         simulated_cell_extract_df, "tp2", drug_name = "drug1")
-    expect_identical(class(gg_rel_heatmap), c("gg", "ggplot"))
+    expect_identical(class(gg_rel_heatmap), c("ggplot2::ggplot", "ggplot", 
+                                              "ggplot2::gg", "S7_object", "gg"))
 })
